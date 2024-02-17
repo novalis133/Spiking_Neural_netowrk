fed68406f7b5025ad55c031426fc99d90d77b889
novalis133 <154033522+novalis133@users.noreply.github.com>
2024-02-17 19:29:28 +0100

    Add files via upload

---
+++ drl_SNN/run.py
@@ -0,0 +1,32 @@
+# Main
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+from s_policy import *
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import gym
+
+env = gym.make('CartPole-v1')
+torch.manual_seed(0)
+
+device = torch.device("cpu")
+
+lif1 = snn.Leaky(beta=0.9, init_hidden=True)
+lif2 = snn.Leaky(beta=0.9, init_hidden=True, output=True)
+
+s_policy = S_Policy()
+policy_optimizer = optim.Adam(s_policy.parameters(), lr=1e-2)
+
+BP_net = nn.Sequential(nn.Flatten(),
+                    nn.Linear(10,32),
+                    lif1,
+                    nn.Linear(32, 2),
+                    lif2).to(device)
+
+snn_optimizer = optim.Adam(BP_net.parameters(), lr=1e-3)  # Notice the different learning rate
+
+
+scores = reinforce(s_policy, s_policy, policy_optimizer, snn_optimizer, env, device)
+print()
